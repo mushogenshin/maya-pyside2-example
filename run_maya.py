92fd9cc02980f6ad82afbb238b8c260484323b20
Mushogenshin <mushogenshin@gmail.com>
2023-06-18 10:11:09 -0500

    cleanup imports

--- run_maya.py
+++ run_maya.py
@@ -4,38 +4,6 @@ import logging
 logger = logging.getLogger(__name__)
 IS_PY2 = True if sys.version_info.major < 3 else False
 
-
-def maya_qt5_main_window():
-    '''
-    Use with Qt5 or PySide2, with support for both Python 2 and 3.
-    :rtype: PySide2.QtWidgets.QWidget | PySide2.QtWidgets.QMainWindow
-    '''
-    logger.info("Getting Maya main window")
-    if IS_PY2:
-        from maya.OpenMayaUI import MQtUtil
-        from shiboken2 import wrapInstance
-        from PySide2.QtWidgets import QWidget
-        main_window_ptr = MQtUtil.mainWindow()
-        return wrapInstance(long(main_window_ptr), QWidget)  # type: ignore
-    else:
-        # `long` is obsolete and `int` doesn't work properly with `wrapInstance`
-        from PySide2.QtWidgets import QApplication
-        # get the QApplication instance if it exists
-        app = QApplication.instance()
-
-        if not app:
-            app = QApplication(sys.argv)
-
-        def maya_window():
-            maya_win = next(w for w in app.topLevelWidgets()
-                            if w.objectName() == 'MayaWindow')
-            return maya_win
-
-        return maya_window()
-
-import sys
-import logging
-
 try:
     import maya.OpenMayaUI as omui
     from PySide2 import QtWidgets, QtCore
@@ -85,6 +53,34 @@ class MyWidget(QtWidgets.QDialog):
     def execute(self):
         print("TODO: Execute something here")
         pass
+
+def maya_qt5_main_window():
+    '''
+    Use with Qt5 or PySide2, with support for both Python 2 and 3.
+    :rtype: PySide2.QtWidgets.QWidget | PySide2.QtWidgets.QMainWindow
+    '''
+    logger.info("Getting Maya main window")
+    if IS_PY2:
+        from maya.OpenMayaUI import MQtUtil
+        from shiboken2 import wrapInstance
+        from PySide2.QtWidgets import QWidget
+        main_window_ptr = MQtUtil.mainWindow()
+        return wrapInstance(long(main_window_ptr), QWidget)  # type: ignore
+    else:
+        # `long` is obsolete and `int` doesn't work properly with `wrapInstance`
+        from PySide2.QtWidgets import QApplication
+        # get the QApplication instance if it exists
+        app = QApplication.instance()
+
+        if not app:
+            app = QApplication(sys.argv)
+
+        def maya_window():
+            maya_win = next(w for w in app.topLevelWidgets()
+                            if w.objectName() == 'MayaWindow')
+            return maya_win
+
+        return maya_window()
 
 def main():
     if MAYA:
